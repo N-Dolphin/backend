addf580bac88c3dbdaafe68f8d75b84d5846ba6f
lsheon0927@naver.com <lsheon0927@naver.com>
2024-08-18 08:30:29 +0900

    #2-2 user테이블 에러 수정
    "user"는 db의 예약어이므로, \"user\" 으로 변경

--- src/main/java/org/example/backend/model/entity/UserEntity.java
+++ src/main/java/org/example/backend/model/entity/UserEntity.java
@@ -13,7 +13,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
-@Table(name = "user")
+@Table(name = "\"user\"")
 
 @Getter
 @Setter
