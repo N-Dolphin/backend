0a137f7ce00758e55270d7e36edaec30d5c8927b
lsheon0927@naver.com <lsheon0927@naver.com>
2024-08-20 15:27:39 +0900

    fix: Test 실패 에러
    인텔리제이 test 환경 변수 추가, test용 yaml파일 추가

--- src/test/java/org/example/backend/BackendApplicationTests.java
+++ src/test/java/org/example/backend/BackendApplicationTests.java
@@ -2,8 +2,11 @@ package org.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // 'test' 프로파일을 활성화합니다.
+
 class BackendApplicationTests {
 
     @Test
