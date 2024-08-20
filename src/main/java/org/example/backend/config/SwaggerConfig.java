c5e1eb4599e099325b8b4fa86f711c9521d084f6
lsheon0927@naver.com <lsheon0927@naver.com>
2024-08-20 15:58:53 +0900

    feat Swagger 관련 설정 추가
    config, yaml

---
+++ src/main/java/org/example/backend/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package org.example.backend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "데이트 어플 API 명세서",
+                description = "API 명세서",
+                version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+    @Bean
+
+    public GroupedOpenApi chatOpenApi() {
+        // "/v1/**" 경로에 매칭되는 API를 그룹화하여 문서화한다.
+        String[] paths = {"/v1/**"};
+
+        return GroupedOpenApi.builder()
+                .group("데이트 어플 API v1")  // 그룹 이름을 설정한다.
+                .pathsToMatch(paths)     // 그룹에 속하는 경로 패턴을 지정한다.
+                .build();
+    }
+}
