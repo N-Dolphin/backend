b91db748fe0d859ccb75fe42bc2ef2049a883085
lsheon0927@naver.com <lsheon0927@naver.com>
2024-08-22 14:43:50 +0900

    User에 저장될 기본 필드 수정

--- src/main/java/org/example/backend/model/entity/Gender.java
+++
@@ -1,8 +0,0 @@
-package org.example.backend.model.entity;
-
-
-public enum Gender {
-	MALE,
-	FEMALE
-}
-
---
+++ src/main/java/org/example/backend/model/entity/enums/Role.java
@@ -0,0 +1,7 @@
+package org.example.backend.model.entity.enums;
+
+
+public enum Role {
+	USER,
+	ADMIN
+}
